6722664fc202e36f7787c2fb7a1eea33fdbbd1a0
Calvin Echols <calvin.echols@turner.com>
2017-06-08 14:36:43 -0400

    added hc

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ router.get('/', function(req, res) {
     res.json({test: 'Hello World!!'});
 });
 
+router.get('/hc/', function(req, res) {
+    res.sendStatus(200);
+});
+
 app.use('/', router);
 
 app.listen(8080, function () {
